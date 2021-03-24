405e7963224a40bf473796b9f9a684a6e40d955d
Amol1419 <68550419+Amol1419@users.noreply.github.com>
2021-03-25 00:11:46 +0530

    Add files via upload

---
+++ Core Java/Java_Assignment1/Avg_marks.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * marks
+ */
+public class marks {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        int phy = scan.nextInt();
+        int chem = scan.nextInt();
+        int math = scan.nextInt();
+        if (phy>60 && chem>60 && math>60){
+            System.out.println("Passed");
+        }
+        else if (((phy+chem)>120) || ((math+phy)>120) || ((math+chem)>120)){
+            System.out.println("Promoted");
+        }
+        else{
+            System.out.println("Failed");
+        }
+    }
+}
